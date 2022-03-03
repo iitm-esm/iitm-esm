babe4d8265860b27af37c300cc7c3686d55ea8a8
prajeeshag <prajeeshag@gmail.com>
2022-03-03 17:34:22 +0530

    cleanup

--- src/mom4p1/exp/doall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -xue
-#mom4p1_coupled_compile.csh
-cp ../exec_ibm/mom4p1_coupled/fms_mom4p1_coupled.x ../../../test_latest_ocn/exec/cfs_ocean_mom4ice
-cp ../exec_ibm/mom4p1_coupled/fms_mom4p1_coupled.x ../../../test_latest_ocn1/exec/cfs_ocean_mom4ice
--- src/mom4p1/exp/make_mppnccombine.sh
+++
@@ -1,5 +0,0 @@
-
-LDFLAGS="-L/gpfs1/home/Libs/INTEL/NETCDF4/netcdf-4.1.3/lib -lnetcdf -lnetcdff"
-INCL="-I/gpfs1/home/Libs/INTEL/NETCDF4/netcdf-4.1.3/include"
-
-icc -O -o ../bin/mppnccombine $INCL $LDFLAGS ../src/postprocessing/mppnccombine/mppnccombine.c
