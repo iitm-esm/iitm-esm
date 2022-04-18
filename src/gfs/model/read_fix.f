1eef0b829e320a592f517393cceeb4e90531d063
prajeeshag <prajeeshag@gmail.com>
2022-04-18 19:46:17 +0530

    No difference

--- src/gfs/model/read_fix.f
+++ src/gfs/model/read_fix.f
@@ -141,21 +141,21 @@
 !        sfc_fld%SMC(k,:,:)=buff3(:,:)
 !      ENDDO
 
-      if(icolor.eq.2.and.me.eq.nodes-1) buff1=data%sheleg
-      call split2d(buff1, buffo,global_lats_r)
-      CALL interpred(1,kmsk,buffo,sfc_fld%SHELEG,
-     &               global_lats_r,lonsperlar)
-
-      DO K = 1, LSOIL
-        if(icolor.eq.2.and.me.eq.nodes-1) buff1=data%stc(:,:,k)
-        call split2d(buff1, buffo,global_lats_r)
-        CALL interpred(1,kmsk,buffo,buff3,global_lats_r,lonsperlar)
-        sfc_fld%STC(k,:,:)=buff3(:,:)
-      ENDDO
-
-      if(icolor.eq.2.and.me.eq.nodes-1) buff1=data%tg3
-      call split2d(buff1, buffo,global_lats_r)
-      CALL interpred(1,kmsk,buffo,sfc_fld%TG3,global_lats_r,lonsperlar)
+!      if(icolor.eq.2.and.me.eq.nodes-1) buff1=data%sheleg
+!      call split2d(buff1, buffo,global_lats_r)
+!      CALL interpred(1,kmsk,buffo,sfc_fld%SHELEG,
+!     &               global_lats_r,lonsperlar)
+!
+!      DO K = 1, LSOIL
+!        if(icolor.eq.2.and.me.eq.nodes-1) buff1=data%stc(:,:,k)
+!        call split2d(buff1, buffo,global_lats_r)
+!        CALL interpred(1,kmsk,buffo,buff3,global_lats_r,lonsperlar)
+!        sfc_fld%STC(k,:,:)=buff3(:,:)
+!      ENDDO
+!
+!      if(icolor.eq.2.and.me.eq.nodes-1) buff1=data%tg3
+!      call split2d(buff1, buffo,global_lats_r)
+!      CALL interpred(1,kmsk,buffo,sfc_fld%TG3,global_lats_r,lonsperlar)
 
       if(icolor.eq.2.and.me.eq.nodes-1) buff1=data%zorl
       call split2d(buff1, buffo,global_lats_r)
